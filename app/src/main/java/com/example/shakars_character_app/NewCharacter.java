5188b8d69439544e0708f2499a97b5a9b19025c1
AngelofDeath666 <khejser@hotmail.com>
2020-11-19 16:23:40 +0100

    new character class wip

--- app/src/main/java/com/example/shakars_character_app/NewCharacter.java
+++ app/src/main/java/com/example/shakars_character_app/NewCharacter.java
@@ -1,6 +1,19 @@
 package com.example.shakars_character_app;
 
-public class NewCharacter {
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewCharacter extends AppCompatActivity {
+    Button save;
+    TextView title;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_character);
+    }
 
 }
