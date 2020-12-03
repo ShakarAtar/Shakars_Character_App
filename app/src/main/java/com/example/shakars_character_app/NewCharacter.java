a98e3e7ed572695748bc14841da960fb678314d4
AngelofDeath666 <khejser@hotmail.com>
2020-12-03 17:15:46 +0100

    Catagories and properties WIP

--- app/src/main/java/com/example/shakars_character_app/NewCharacter.java
+++ app/src/main/java/com/example/shakars_character_app/NewCharacter.java
@@ -10,36 +10,49 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class NewCharacter extends AppCompatActivity implements View.OnClickListener {
     Button save;
     TextView title;
+
+    static class CategoriesAndProperties {
+        List<Integer> categories = Arrays.asList(R.string.basic, R.string.looks);
+
+        List<List<Integer>> properties = Arrays.asList(
+                Arrays.asList(R.string.name, R.string.titleKenningMoniker, R.string.race, R.string.gender, R.string.age, R.string.nationality, R.string.hometown, R.string.continent, R.string.rpClass, R.string.sexuality, R.string.occupation, R.string.religion),
+                Arrays.asList(R.string.name, R.string.titleKenningMoniker, R.string.race, R.string.gender, R.string.age, R.string.nationality, R.string.hometown, R.string.continent, R.string.rpClass, R.string.sexuality, R.string.occupation, R.string.religion)
+);
+    }
+
+    CategoriesAndProperties data = new CategoriesAndProperties();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_character);
 
+        title = findViewById(R.id.newCharacterTitle);
+
         //Buttons
         save = findViewById(R.id.newCharacterSaveButton);
         save.setOnClickListener(this);
 
-        title = findViewById(R.id.newCharacterTitle);
-        title.setOnClickListener(this);
-
         ViewGroup content = findViewById(R.id.newCharacterLayout);
+        content.removeAllViews();
 
         for (int i = 0; i < titles.length; i++) {
             View root = getLayoutInflater().inflate(R.layout.new_character_basic_info, content, false);
             root.findViewById(R.id.newCharacterTVTitles).setOnClickListener(this);
             root.findViewById(R.id.newCharacterHints).setOnClickListener(this);
             TextView titleTV = root.findViewById(R.id.newCharacterTVTitles);
-            titleTV.setText(titles[i]);
+            titleTV.setText(titles2[i]);
             titleTV.setTag(i);
             titlesTV.add(titleTV);
 
             EditText hintTV = root.findViewById(R.id.newCharacterHints);
-            hintTV.setText(hints[i]);
+            hintTV.setHint(hints[i]);
             hintTV.setTag(i);
             hintsTV.add(hintTV);
 
@@ -49,6 +62,7 @@ public class NewCharacter extends AppCompatActivity implements View.OnClickListe
 
         }
 
+    int[] titles2 = { R.string.name, R.string.titleKenningMoniker, R.string.race, R.string.gender, R.string.age, R.string.nationality, R.string.hometown, R.string.continent, R.string.rpClass, R.string.sexuality, R.string.occupation, R.string.religion};
         String[] titles = {"@string/name", "@string/titleKenningMoniker", "@string/race", "@string/gender", "@string/age", "@string/nationality", "@string/hometown", "@string/continent", "@string/rpClass", "@string/sexuality", "@string/occupation", "@string/religion"};
         String[] hints = {"@string/nameSuggestion", "@string/titleKenningMonikerSuggestion", "@string/raceSuggestion", "@string/genderSuggestion", "@string/ageSuggestion", "@string/nationalitySuggestion", "@string/hometownSuggestion", "@string/continentSuggestion", "@string/rpClassSuggestion", "@string/sexualitySuggestion", "@string/occupationSuggestion", "@string/religionSuggestion"};
         ArrayList<View> titlesTV = new ArrayList<>();
