4206175fe04c8a37bf375c147a62f43066aa58a8
AngelofDeath666 <khejser@hotmail.com>
2020-12-03 11:11:29 +0100

    New character array viewing in progress

--- app/src/main/java/com/example/shakars_character_app/NewCharacter.java
+++ app/src/main/java/com/example/shakars_character_app/NewCharacter.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -30,7 +31,17 @@ public class NewCharacter extends AppCompatActivity implements View.OnClickListe
 
         for (int i = 0; i < titles.length; i++) {
             View root = getLayoutInflater().inflate(R.layout.new_character_basic_info, content, false);
-            root.findViewById()
+            root.findViewById(R.id.newCharacterTVTitles).setOnClickListener(this);
+            root.findViewById(R.id.newCharacterHints).setOnClickListener(this);
+            TextView titleTV = root.findViewById(R.id.newCharacterTVTitles);
+            titleTV.setText(titles[i]);
+            titleTV.setTag(i);
+            titlesTV.add(titleTV);
+
+            EditText hintTV = root.findViewById(R.id.newCharacterHints);
+            hintTV.setText(hints[i]);
+            hintTV.setTag(i);
+            hintsTV.add(hintTV);
 
             content.addView(root);
             }
@@ -40,7 +51,8 @@ public class NewCharacter extends AppCompatActivity implements View.OnClickListe
 
         String[] titles = {"@string/name", "@string/titleKenningMoniker", "@string/race", "@string/gender", "@string/age", "@string/nationality", "@string/hometown", "@string/continent", "@string/rpClass", "@string/sexuality", "@string/occupation", "@string/religion"};
         String[] hints = {"@string/nameSuggestion", "@string/titleKenningMonikerSuggestion", "@string/raceSuggestion", "@string/genderSuggestion", "@string/ageSuggestion", "@string/nationalitySuggestion", "@string/hometownSuggestion", "@string/continentSuggestion", "@string/rpClassSuggestion", "@string/sexualitySuggestion", "@string/occupationSuggestion", "@string/religionSuggestion"};
-        ArrayList<View> titlesTv = new ArrayList<>();
+        ArrayList<View> titlesTV = new ArrayList<>();
+        ArrayList<View> hintsTV = new ArrayList<>();
 
         @Override
         public void onClick (View v){
