4a843a82fe6df87f0dc238180fdd127f348a28b2
AngelofDeath666 <khejser@hotmail.com>
2020-12-03 10:52:56 +0100

    New character array viewing in progress, commit before deleting the textViews

--- app/src/main/java/com/example/shakars_character_app/NewCharacter.java
+++ app/src/main/java/com/example/shakars_character_app/NewCharacter.java
@@ -2,10 +2,13 @@ package com.example.shakars_character_app;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class NewCharacter extends AppCompatActivity implements View.OnClickListener {
     Button save;
@@ -23,10 +26,25 @@ public class NewCharacter extends AppCompatActivity implements View.OnClickListe
         title = findViewById(R.id.newCharacterTitle);
         title.setOnClickListener(this);
 
+        ViewGroup content = findViewById(R.id.newCharacterLayout);
+
+        for (int i = 0; i < titles.length; i++) {
+            View root = getLayoutInflater().inflate(R.layout.new_character_basic_info, content, false);
+            root.findViewById()
+
+            content.addView(root);
+            }
+
+
+        }
+
+        String[] titles = {"@string/name", "@string/titleKenningMoniker", "@string/race", "@string/gender", "@string/age", "@string/nationality", "@string/hometown", "@string/continent", "@string/rpClass", "@string/sexuality", "@string/occupation", "@string/religion"};
+        String[] hints = {"@string/nameSuggestion", "@string/titleKenningMonikerSuggestion", "@string/raceSuggestion", "@string/genderSuggestion", "@string/ageSuggestion", "@string/nationalitySuggestion", "@string/hometownSuggestion", "@string/continentSuggestion", "@string/rpClassSuggestion", "@string/sexualitySuggestion", "@string/occupationSuggestion", "@string/religionSuggestion"};
+        ArrayList<View> titlesTv = new ArrayList<>();
+
+        @Override
+        public void onClick (View v){
+
+        }
     }
 
-    @Override
-    public void onClick(View v) {
-
-    }
-}
