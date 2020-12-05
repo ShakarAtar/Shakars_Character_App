3f9ee91e90c75359b0cef107c6ba1c84a4bb96de
AngelofDeath666 <khejser@hotmail.com>
2020-12-05 20:11:41 +0100

    CatagoriesAndProperties PC and NPC arrays done

--- app/src/main/java/com/example/shakars_character_app/NewCharacter.java
+++ app/src/main/java/com/example/shakars_character_app/NewCharacter.java
@@ -40,51 +40,54 @@ public class NewCharacter extends AppCompatActivity implements View.OnClickListe
             catTitleTv.setTag(catIndex);
             content.addView(catRoot);
             categorieisLL.add(cat_title_ll);
-            cat_title_ll.setVisibility(CatagoriesAndProperties.folded[catIndex] ? View.GONE : View.VISIBLE );
+            cat_title_ll.setVisibility(CatagoriesAndProperties.folded[catIndex] ? View.GONE : View.VISIBLE);
 
+            View root = getLayoutInflater().inflate(R.layout.category_property, content, false);
             int[] props = CatagoriesAndProperties.data.propertiesPC[catIndex];
             for (int propIndex = 0; propIndex < props.length; propIndex++) {
 
-                View root = getLayoutInflater().inflate(R.layout.category_proterty, content, false);
                 TextView titleTV = root.findViewById(R.id.newCharacterTVTitles);
                 titleTV.setText(props[propIndex]);
 
-                EditText hintTV = root.findViewById(R.id.newCharacterHints);
-                hintTV.setHint(hints[propIndex]);
-                hintTV.setTag(propIndex);
-                hintsTV.add(hintTV);
-                cat_title_ll.addView(root);
 
             }
-        }
-    }
 
+            int[] hints = CatagoriesAndProperties.data.hintsPC[catIndex];
+            for (int hintIndex = 0; hintIndex < hints.length; hintIndex++) {
+                EditText hintTV = root.findViewById(R.id.newCharacterHints);
+                hintTV.setHint(hints[hintIndex]);
+                hintTV.setTag(hintIndex);
+//                hintsTV.add(hintTV);
 
-
-    @Override
-    public void onClick (View v){
-        int catIndex = (int) v.getTag();
-        CatagoriesAndProperties.folded[catIndex] = !CatagoriesAndProperties.folded[catIndex];
-        //categorieisLL.get(catIndex).setVisibility( foldetIn[catIndex] ? View.GONE : View.VISIBLE );
-
-        View cat = categorieisLL.get(catIndex);
-        if (CatagoriesAndProperties.folded[catIndex]) {
-            cat.animate().scaleY(0);
-
-        } else {
-            cat.setVisibility( View.VISIBLE );
-            cat.animate().scaleY(1);
-
+            }
+            cat_title_ll.addView(root);
         }
 
     }
 
 
-    int[] titles2 = { R.string.name, R.string.titleKenningMoniker, R.string.race, R.string.gender, R.string.age, R.string.nationality, R.string.hometown, R.string.continent, R.string.rpClass, R.string.sexuality, R.string.occupation, R.string.religion};
-        String[] titles = {"@string/name", "@string/titleKenningMoniker", "@string/race", "@string/gender", "@string/age", "@string/nationality", "@string/hometown", "@string/continent", "@string/rpClass", "@string/sexuality", "@string/occupation", "@string/religion"};
-        String[] hints = {"@string/nameSuggestion", "@string/titleKenningMonikerSuggestion", "@string/raceSuggestion", "@string/genderSuggestion", "@string/ageSuggestion", "@string/nationalitySuggestion", "@string/hometownSuggestion", "@string/continentSuggestion", "@string/rpClassSuggestion", "@string/sexualitySuggestion", "@string/occupationSuggestion", "@string/religionSuggestion"};
+        @Override
+        public void onClick (View v){
+            int catIndex = (int) v.getTag();
+            CatagoriesAndProperties.folded[catIndex] = !CatagoriesAndProperties.folded[catIndex];
+            //categorieisLL.get(catIndex).setVisibility( foldetIn[catIndex] ? View.GONE : View.VISIBLE );
+
+            View cat = categorieisLL.get(catIndex);
+            if (CatagoriesAndProperties.folded[catIndex]) {
+                cat.animate().scaleY(0);
+
+            } else {
+                cat.setVisibility(View.VISIBLE);
+                cat.animate().scaleY(1);
+
+            }
+
+        }
+
+
         ArrayList<View> categorieisLL = new ArrayList<>();
         ArrayList<View> hintsTV = new ArrayList<>();
 
-    }
 
+
+}
