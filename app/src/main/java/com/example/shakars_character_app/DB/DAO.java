9bb755a56e9d9b2272e374c427dddb768dd2c880
AngelofDeath666 <khejser@hotmail.com>
2021-01-08 16:53:23 +0100

    Firestore DB added to the project and DAO class created

---
+++ app/src/main/java/com/example/shakars_character_app/DB/DAO.java
@@ -0,0 +1,11 @@
+package com.example.shakars_character_app.DB;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class DAO {
+
+    //Google Firestore Databse Access
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
+}
